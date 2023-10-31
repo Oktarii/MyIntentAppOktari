925187dfe8f112ce81a32b6450123819c0b72163
AmiliaKartika <iamtwomlks@gmail.com>
2023-10-31 13:57:53 +0700

    Initial commit

---
+++ MyIntentApp_Oktari/app/src/main/java/com/example/myintentappoktari/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myintentappoktari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
+
+        Button btnMoveWithObject = findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_move_activity) {
+            Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+            startActivity(moveIntent);
+        } else if (v.getId() == R.id.btn_move_activity_data) {
+            Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Oktari Girl");
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 19);
+            startActivity(moveWithDataIntent);
+        }else if (v.getId() == R.id.btn_move_activity_object) {
+            Person person = new Person();
+            person.setName("Oktari Jurusan SIREG 5A");
+            person.setAge(19);
+            person.setEmail("09031182126028@ilkom.unsri.ac.id");
+            person.setCity("Palembang");
+
+            Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
+            moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
+            startActivity(moveWithObjectIntent);
+        }
+    }
+}
